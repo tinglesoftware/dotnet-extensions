2fe5e95ac3b70455a6eef628f0d2607395dd28e7
Maxwell Weru <mburumaxwell@gmail.com>
2021-10-29 17:22:54 +0300

    Mark JsonClone(...) and JsonConvertTo(...) as obsolete

--- src/Tingle.Extensions.Json/ObjectExtensions.cs
+++ src/Tingle.Extensions.Json/ObjectExtensions.cs
@@ -15,7 +15,7 @@ namespace System
         /// <typeparam name="T"></typeparam>
         /// <param name="source">the source of the data</param>
         /// <param name="options">the serialization settings if any</param>
-        /// <returns></returns>
+        [Obsolete("Use record types instead")]
         public static T? JsonClone<T>(this T? source, JsonSerializerOptions? options = null)
             where T : class
         {
@@ -48,6 +48,7 @@ namespace System
         /// <param name="source">the source of the data</param>
         /// <param name="options">the serialization settings if any</param>
         /// <returns></returns>
+        [Obsolete("Use record types instead")]
         public static T? JsonConvertTo<T>(this object? source, JsonSerializerOptions? options = null)
         {
             if (source == default) return default;
