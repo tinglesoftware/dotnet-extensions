9da5ee819ce6b3fddbfe03ca9a0659ef6090a829
Maxwell Weru <mburumaxwell@gmail.com>
2021-10-29 17:23:48 +0300

    Remove commented out JsonCopyTo(...) method

--- src/Tingle.Extensions.Json/ObjectExtensions.cs
+++ src/Tingle.Extensions.Json/ObjectExtensions.cs
@@ -24,23 +24,6 @@ namespace System
             return JsonSerializer.Deserialize<T>(json, options ?? JsonHelper.DefaultOptions);
         }
 
-        // TODO: enable once officially supported https://github.com/dotnet/runtime/issues/29538
-
-        ///// <summary>
-        ///// Copy data from one object to another via JSON serializer
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="source">the source of the data</param>
-        ///// <param name="target"></param>
-        ///// <param name="options">the serialization settings if any</param>
-        //public static void JsonCopyTo<T>(this T source, T target, JsonSerializerOptions options = null)
-        //    where T : class
-        //{
-        //    if (source == default || target == default) return;
-        //    var json = JsonSerializer.Serialize(source, options ?? JsonHelper.defaultOptions);
-        //    JsonSerializer.PopulateObject(json, target, options ?? JsonHelper.defaultOptions);
-        //}
-
         /// <summary>
         /// Convert one object to another via JSON serializer
         /// </summary>
