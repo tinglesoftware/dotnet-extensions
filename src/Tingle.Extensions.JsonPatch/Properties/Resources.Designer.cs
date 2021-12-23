5aabb9d904ad9ae05a5687ee92ada86ada946114
Maxwell Weru <mburumaxwell@gmail.com>
2021-12-23 19:26:10 +0300

    Fix Resources

--- src/Tingle.Extensions.JsonPatch/Properties/Resources.Designer.cs
+++ src/Tingle.Extensions.JsonPatch/Properties/Resources.Designer.cs
@@ -20,7 +20,7 @@ namespace Tingle.Extensions.JsonPatch.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -44,7 +44,7 @@ namespace Tingle.Extensions.JsonPatch.Properties {
             {
                 if (object.ReferenceEquals(resourceMan, null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tingle.JsonPatch.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tingle.Extensions.JsonPatch.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
