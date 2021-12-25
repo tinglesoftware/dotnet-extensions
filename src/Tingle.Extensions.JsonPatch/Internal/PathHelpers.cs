a765b5f8617a961c1742d99413786a3133ec15c3
Maxwell Weru <mburumaxwell@gmail.com>
2021-12-25 17:50:39 +0300

    Use single quotes for characters

--- src/Tingle.Extensions.JsonPatch/Internal/PathHelpers.cs
+++ src/Tingle.Extensions.JsonPatch/Internal/PathHelpers.cs
@@ -11,7 +11,7 @@ internal static class PathHelpers
         // absolutely necessary, but it allows us to already catch mistakes
         // on creation of the patch document rather than on execute.
 
-        if (path.Contains(".") || path.Contains("//") || path.Contains(" ") || path.Contains("\\"))
+        if (path.Contains('.') || path.Contains("//") || path.Contains(' ') || path.Contains('\\'))
         {
             throw new JsonPatchException(Resources.FormatInvalidValueForPath(path), null);
         }
