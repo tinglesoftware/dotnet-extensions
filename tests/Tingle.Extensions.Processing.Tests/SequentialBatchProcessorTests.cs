86d1cf8855be322e62e562636d7cc14efaa6dc51
Maxwell Weru <mburumaxwell@gmail.com>
2022-05-09 13:04:52 +0300

    Processing updates (#67)
    Updates to the `Tingle.Extensions.Processing` libraries including:
    - Use `IEnumerable<T>` in `SplitAndBatchProcessor<T>`.
    - Use `ConfigureAwait(false)` in `HandleAsync(...)`.
    - Support use of Chunk for .NET 6 and up.
    - Added unit tests.
    - Updated XML comments.

---
+++ tests/Tingle.Extensions.Processing.Tests/SequentialBatchProcessorTests.cs
@@ -0,0 +1,38 @@
+﻿namespace Tingle.Extensions.Processing.Tests;
+
+public class SequentialBatchProcessorTests
+{
+    [Fact]
+    public async Task ProcessAsync_Works_Concurrency_1()
+    {
+        var numbers = Enumerable.Range(1, 100);
+        var invocations = 0;
+        var processed = new List<int>();
+        var processor = new SequentialBatchProcessor<int>(5, (n, ct) =>
+        {
+            Interlocked.Increment(ref invocations);
+            processed.Add(n);
+            return Task.CompletedTask;
+        });
+        await processor.ProcessAsync(numbers);
+        Assert.Equal(100, invocations);
+        Assert.Equal(numbers, processed);
+    }
+
+    [Fact]
+    public async Task ProcessAsync_Works_Concurrency_3()
+    {
+        var numbers = Enumerable.Range(1, 100);
+        var invocations = 0;
+        var processed = new System.Collections.Concurrent.ConcurrentBag<int>();
+        var processor = new SequentialBatchProcessor<int>(3, async (n, ct) =>
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(new Random(Guid.NewGuid().GetHashCode()).Next(1, 10)), ct);
+            Interlocked.Increment(ref invocations);
+            processed.Add(n);
+        });
+        await processor.ProcessAsync(numbers);
+        Assert.Equal(100, invocations);
+        Assert.Equal(numbers, processed.OrderBy(x => x));
+    }
+}
---
+++ tests/Tingle.Extensions.Processing.Tests/SplitAndBatchProcessorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Tingle.Extensions.Processing.Tests;
+
+public class SplitAndBatchProcessorTests
+{
+    [Fact]
+    public async Task ProcessAsync_Works()
+    {
+        var numbers = Enumerable.Range(1, 100);
+        var invocations = 0;
+        var processed = new System.Collections.Concurrent.ConcurrentBag<int>();
+        var processor = new SplitAndBatchProcessor<int>(5, async (b, ct) =>
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(new Random(Guid.NewGuid().GetHashCode()).Next(1, 400)), ct);
+            Interlocked.Increment(ref invocations);
+            foreach (var n in b) processed.Add(n);
+        });
+        await processor.ProcessAsync(numbers);
+        Assert.Equal(20, invocations);
+        Assert.Equal(numbers, processed.OrderBy(x => x));
+    }
+}
