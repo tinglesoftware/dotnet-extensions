0425d43115fddb17476fc800493b48b64ec482ee
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-30 12:31:31 +0300

    Added sample for using MongoCache with AspNetCore SessionStore

---
+++ samples/AspNetCoreSessionState/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AspNetCoreSessionState.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreSessionState.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        await HttpContext.Session.LoadAsync();
+        var data = HttpContext.Session.GetString("data");
+        if (data == null)
+        {
+            HttpContext.Session.SetString("data", "someValue");
+        }
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        ViewData["Message"] = "Your application description page.";
+
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        ViewData["Message"] = "Your contact page.";
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ samples/AspNetCoreSessionState/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+builder.Services.AddMongoCache(builder.Configuration.GetConnectionString("Mongo"));
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(3600);
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseCookiePolicy();
+app.UseSession();
+
+app.MapDefaultControllerRoute();
+
+await app.RunAsync();
