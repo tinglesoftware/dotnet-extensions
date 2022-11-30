67a06fda8dbbf9a8145a38725b33018513f6a9b2
Maxwell Weru <mburumaxwell@gmail.com>
2022-11-30 13:11:05 +0300

    Better initialization of the cache with support for IOptionsMonitor<MongoCacheOptions>

--- src/Tingle.Extensions.Caching.MongoDB/MongoCache.cs
+++ src/Tingle.Extensions.Caching.MongoDB/MongoCache.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using MongoDB.Driver.Linq;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Tingle.Extensions.Caching.MongoDB;
 
@@ -11,7 +12,9 @@ namespace Tingle.Extensions.Caching.MongoDB;
 public class MongoCache : IDistributedCache
 {
     private readonly SemaphoreSlim connectionLock = new(initialCount: 1, maxCount: 1);
-    private readonly MongoCacheOptions options;
+    private readonly IDisposable? monitorListener;
+    private MongoCacheOptions options;
+    private bool initializedClient;
     private IMongoClient? client;
     private IMongoCollection<MongoCacheEntry>? collection;
 
@@ -21,34 +24,26 @@ public class MongoCache : IDistributedCache
     /// <param name="optionsAccessor">Options accessor.</param>
     public MongoCache(IOptions<MongoCacheOptions> optionsAccessor)
     {
-        if (optionsAccessor == null)
+        Initialize(optionsAccessor);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MongoCache"/> class.
+    /// </summary>
+    /// <remarks>
+    /// Using the <see cref="IOptionsMonitor{T}"/> would make the internal client reference to be updated if any of the options change.
+    /// </remarks>
+    /// <param name="optionsMonitor">Options monitor.</param>
+    public MongoCache(IOptionsMonitor<MongoCacheOptions> optionsMonitor)
+    {
+        if (optionsMonitor == null)
         {
-            throw new ArgumentNullException(nameof(optionsAccessor));
+            throw new ArgumentNullException(nameof(optionsMonitor));
         }
 
-        options = optionsAccessor.Value;
+        Initialize(optionsMonitor.CurrentValue);
 
-        if (string.IsNullOrWhiteSpace(options.ConnectionString) && options.MongoClient == null)
-        {
-            throw new InvalidOperationException($"You need to specify either a {nameof(options.ConnectionString)} or an existing {nameof(options.MongoClient)} in the {nameof(MongoCacheOptions)}.");
-        }
-
-        // attempt to pull from the database name from the connection string
-        if (string.IsNullOrWhiteSpace(options.DatabaseName) && !string.IsNullOrWhiteSpace(options.ConnectionString))
-        {
-            var url = new MongoUrl(options.ConnectionString);
-            options.DatabaseName = url.DatabaseName;
-        }
-
-        if (string.IsNullOrWhiteSpace(options.DatabaseName))
-        {
-            throw new InvalidOperationException($"You need to specify either a database name in the {nameof(options.ConnectionString)} or the {nameof(MongoCacheOptions)}.");
-        }
-
-        if (string.IsNullOrWhiteSpace(options.CollectionName))
-        {
-            throw new InvalidOperationException($"You need to specify either a collection name in the {nameof(options.ConnectionString)} or the {nameof(MongoCacheOptions)}.");
-        }
+        monitorListener = optionsMonitor.OnChange(OnOptionsChange);
     }
 
     /// <inheritdoc/>
@@ -236,6 +231,38 @@ public class MongoCache : IDistributedCache
         return absoluteExpiration;
     }
 
+    [MemberNotNull(nameof(options))]
+    private void Initialize(IOptions<MongoCacheOptions> optionsAccessor)
+    {
+        if (optionsAccessor == null)
+        {
+            throw new ArgumentNullException(nameof(optionsAccessor));
+        }
+
+        if (string.IsNullOrWhiteSpace(optionsAccessor.Value.ConnectionString) && optionsAccessor.Value.MongoClient == null)
+        {
+            throw new InvalidOperationException("You need to specify either a ConnectionString or an existing MongoClient in the options.");
+        }
+
+        // attempt to pull from the database name from the connection string
+        if (string.IsNullOrWhiteSpace(optionsAccessor.Value.DatabaseName) && !string.IsNullOrWhiteSpace(optionsAccessor.Value.ConnectionString))
+        {
+            var url = new MongoUrl(optionsAccessor.Value.ConnectionString);
+            optionsAccessor.Value.DatabaseName = url.DatabaseName;
+        }
+
+        if (string.IsNullOrWhiteSpace(optionsAccessor.Value.DatabaseName))
+        {
+            throw new InvalidOperationException("You need to specify either a DatabaseName or a ConnectionString with the DatabaseName in the options.");
+        }
+
+        if (string.IsNullOrWhiteSpace(optionsAccessor.Value.CollectionName))
+        {
+            throw new InvalidOperationException("You need to specify either a CollectionName in the options.");
+        }
+
+        options = optionsAccessor.Value;
+    }
     private async Task ConnectAsync(CancellationToken token = default)
     {
         token.ThrowIfCancellationRequested();
@@ -268,8 +295,27 @@ public class MongoCache : IDistributedCache
         }
     }
 
+    private void OnOptionsChange(MongoCacheOptions options)
+    {
+        // Did we create our own internal client? If so, we need to dispose it.
+        if (initializedClient && client != null)
+        {
+            // In case this becomes an issue with concurrent access to the client, we can see if ReaderWriterLockSlim can be leveraged.
+            if (client is IDisposable d)
+            {
+                d.Dispose();
+            }
+        }
+
+        this.options = options;
+
+        // Force re-initialization on the next Connect
+        collection = null;
+    }
+
     private async Task<IMongoCollection<MongoCacheEntry>> MongoCollectionInitializeAsync()
     {
+        initializedClient = options.MongoClient == null;
         client = GetClientInstance();
 
         var database = client.GetDatabase(options.DatabaseName);
@@ -333,8 +379,7 @@ public class MongoCache : IDistributedCache
 
         if (string.IsNullOrWhiteSpace(options.ConnectionString))
         {
-            throw new ArgumentException($"'{nameof(options.ConnectionString)}' cannot be null or whitespace",
-                                        nameof(options.ConnectionString));
+            throw new InvalidOperationException($"'{nameof(options)}.{nameof(options.ConnectionString)}' cannot be null or whitespace");
         }
 
         return new MongoClient(options.ConnectionString);
