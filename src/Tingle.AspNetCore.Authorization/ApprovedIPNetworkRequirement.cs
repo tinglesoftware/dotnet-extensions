024c5c2bde52fe3fb7c0f009480820043c6a82de
Maxwell Weru <mburumaxwell@gmail.com>
2023-01-02 16:10:43 +0300

    Merge in dotnet-aspnetcore repository (#105)
    This merges in the `dotnet-aspnetcore` repository into this repository for easier management and reduce repetitive tasks. It works mainly because the code is basic functionality that complements the dotnet and aspnetcore frameworks.

---
+++ src/Tingle.AspNetCore.Authorization/ApprovedIPNetworkRequirement.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Net;
+
+namespace Tingle.AspNetCore.Authorization;
+
+/// <summary>
+/// An <see cref="IAuthorizationRequirement"/> that contains IP networks
+/// </summary>
+public sealed class ApprovedIPNetworkRequirement : IAuthorizationRequirement
+{
+    private readonly IList<IPNetwork> networks;
+
+    /// <summary>
+    /// Creates and instance of <see cref="ApprovedIPNetworkRequirement"/>
+    /// </summary>
+    /// <param name="networks">the networks allowed</param>
+    public ApprovedIPNetworkRequirement(IList<IPNetwork> networks)
+    {
+        this.networks = networks ?? throw new ArgumentNullException(nameof(networks));
+    }
+
+    /// <summary>
+    /// Checks is an instance of <see cref="IPAddress"/> is approved
+    /// </summary>
+    /// <param name="address">The address to check.</param>
+    /// <returns></returns>
+    public bool IsApproved(IPAddress address)
+    {
+        // if the IP is an IPv4 mapped to IPv6, remap it
+        var addr = address;
+        if (addr.IsIPv4MappedToIPv6)
+        {
+            addr = addr.MapToIPv4();
+        }
+
+        return networks.Contains(addr);
+    }
+}
---
+++ tests/Tingle.AspNetCore.DataProtection.MongoDB.Tests/MongoDbFixture.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Driver;
+
+namespace Tingle.AspNetCore.DataProtection.MongoDB.Tests;
+
+public sealed class MongoDbFixture : IDisposable
+{
+    private readonly string dbName;
+    private readonly IMongoClient client;
+
+    public MongoDbFixture()
+    {
+        dbName = Guid.NewGuid().ToString();
+        var mub = new MongoUrlBuilder()
+        {
+            Server = MongoServerAddress.Parse("localhost:27017"),
+            DatabaseName = dbName
+        };
+        ConnectionString = mub.ToString();
+        client = new MongoClient(ConnectionString);
+    }
+
+    public string ConnectionString { get; private set; }
+
+    public IMongoClient Client => client;
+
+    public IMongoDatabase Database => client.GetDatabase(dbName);
+
+    public IMongoCollection<T> GetCollection<T>(string? name = null) => Database.GetCollection<T>(name ?? typeof(T).Name);
+
+    public string DatabaseName => dbName;
+
+    #region IDisposable Support
+    private bool disposed = false; // To detect redundant calls
+
+    private void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                client.DropDatabase(dbName);
+            }
+
+            disposed = true;
+        }
+    }
+
+    // This code added to correctly implement the disposable pattern.
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        Dispose(true);
+    }
+    #endregion
+}
