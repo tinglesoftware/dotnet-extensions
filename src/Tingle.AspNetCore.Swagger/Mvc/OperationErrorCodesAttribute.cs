0c09969a0cd2c31a93348ad40a9e80803f87bef9
Maxwell Weru <1645026+mburumaxwell@users.noreply.github.com>
2026-02-02 19:17:00 +0000

    Remove projects no longer released (#547)
    Tingle.AspNetCore.ApplicationInsights, Tingle.AspNetCore.Swagger, and Tingle.AspNetCore.Serilog are no longer released, hence remove their code

--- src/Tingle.AspNetCore.Swagger/Mvc/OperationErrorCodesAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.AspNetCore.Mvc;
-
-/// 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-public sealed class OperationErrorCodesAttribute(params string[] errors) : Attribute
-{
-    /// 
-    public string[] Errors { get; set; } = errors;
-}
